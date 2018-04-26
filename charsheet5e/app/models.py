9944074ab56ff6bc1dc65f3c1af07e55660cbc5e
danielarnarsson <danielarnarsson@nemi.tskoli.is>
2018-04-26 13:51:34 +0000

    Created database

---
+++ charsheet5e/app/models.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+from app import db, login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(254), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    char_sheets = db.relationship('Char_sheet', backref='user', lazy='joined')
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+class Char_sheet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    path = db.Column(db.String(255), index=True, unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Character sheet {}>'.format(self.char_sheet_path)
--- charsheet5e/app/routes.py
+++ charsheet5e/app/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
 from app.forms import LoginForm, RegistrationForm
-from app.models import User, Post
+from app.models import User, Char_sheet
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from werkzeug.urls import url_parse
@@ -16,10 +16,14 @@ def index():
 def login():
     return render_template("login.html", title='Log in')
 
+
 @app.route('/signup')
-def login():
+def signup():
     return render_template("login.html", title='Sign up')
+
 
 @app.route('/contact')
 def contact():
     return render_template("contact.html", title='Contact us')
+
+
