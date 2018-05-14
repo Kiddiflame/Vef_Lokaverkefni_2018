de4f722bbc5c5af6c5885ac350ea05b70a093784
Kiddiflame <kiddiflame@gmail.com>
2018-05-14 11:59:25 +0000

    Delete run.py

--- run.py
+++
@@ -1,6 +0,0 @@
-from app import app, db
-from app.models import User, Char_sheet
-
-@app.shell_context_processor
-def make_shell_context():
-	return {'db': db, 'User': User, 'Char_sheet': Char_sheet}
